ab0c17f23271fdf622344a8551526f4588a8e761
Rat <lukas@klegraefe.de>
2021-07-12 20:58:08 +0200

    Added Minigames and Snake Folder

---
+++ Game_Client/src/main/java/io/github/minetrinity/game/graphics/gui/ingame/minigames/snake/Snake.java
@@ -0,0 +1,23 @@
+package io.github.minetrinity.game.graphics.gui.ingame.minigames.snake;
+
+
+import io.github.minetrinity.game.graphics.gui.GUI;
+
+import java.awt.*;
+
+public class Snake extends GUI {
+    @Override
+    public void open() {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
