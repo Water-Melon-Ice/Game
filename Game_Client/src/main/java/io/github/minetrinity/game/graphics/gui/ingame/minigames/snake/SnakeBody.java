0530a530c6f553fc3063856b7808eecdda42ea28
Rat <lukas@klegraefe.de>
2021-07-13 12:40:13 +0200

    Added SnakeBody and SnakeMain

---
+++ Game_Client/src/main/java/io/github/minetrinity/game/graphics/gui/ingame/minigames/snake/SnakeBody.java
@@ -0,0 +1,12 @@
+package io.github.minetrinity.game.graphics.gui.ingame.minigames.snake;
+
+import java.util.ArrayList;
+import java.awt.Point;
+import java.util.List;
+
+public class SnakeBody {
+
+    private List<Point> SnakeBody = new ArrayList();
+    private Point snakeHead;
+
+}
---
+++ Game_Client/src/main/java/io/github/minetrinity/game/graphics/gui/ingame/minigames/snake/SnakeMain.java
@@ -0,0 +1,52 @@
+package io.github.minetrinity.game.graphics.gui.ingame.minigames.snake;
+
+
+import io.github.minetrinity.game.graphics.gui.GUI;
+
+import java.awt.*;
+
+public class SnakeMain extends GUI {
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMNS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH / ROWS;
+    //private static final String [] FOODS_IMAGE = new String[];
+
+    private static final int RIGHT = 0;
+    private static final int LEFT = 1;
+    private static final int UP = 2;
+    private static final int DOWN = 3;
+
+    private Image FoodImage;
+    private int FoodX;
+    private int FoodY;
+    private boolean gameOer;
+    private int currentDirection;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Snake");
+        
+
+    }
+
+
+
+
+    @Override
+    public void open() {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
