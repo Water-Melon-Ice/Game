677b467011dce18148b4faa0670ce4611561b474
Minetrinity <adrian.dev@outlook.de>
2021-09-10 08:20:47 +0200

    FPS display

--- Game_Core/src/main/java/io/github/minetrinity/game/graphics/Texture.java
+++ Game_Core/src/main/java/io/github/minetrinity/game/graphics/Texture.java
@@ -3,6 +3,7 @@ package io.github.minetrinity.game.graphics;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -20,6 +21,49 @@ public class Texture implements Paintable{
         bGr.drawImage(i, 0, 0, null);
         bGr.dispose();
         return bimg;
+    }
+
+    private static int[][] toARGBIntArray(BufferedImage image) {
+
+        final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+
+        int[][] result = new int[width][height];
+        if (hasAlphaChannel) {
+            final int pixelLength = 4;
+            for (int pixel = 0, row = 0, col = 0; pixel + 3 < pixels.length; pixel += pixelLength) {
+                int argb = 0;
+                argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
+                argb += ((int) pixels[pixel + 1] & 0xff); // blue
+                argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
+                argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
+                result[col][row] = argb;
+                col++;
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        } else {
+            final int pixelLength = 3;
+            for (int pixel = 0, row = 0, col = 0; pixel + 2 < pixels.length; pixel += pixelLength) {
+                int argb = 0;
+                argb += -16777216; // 255 alpha
+                argb += ((int) pixels[pixel] & 0xff); // blue
+                argb += (((int) pixels[pixel + 1] & 0xff) << 8); // green
+                argb += (((int) pixels[pixel + 2] & 0xff) << 16); // red
+                result[col][row] = argb;
+                col++;
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+
+        return result;
     }
 
     private Image img;
@@ -77,10 +121,11 @@ public class Texture implements Paintable{
         BufferedImage img = getBufferedImage();
         BufferedImage out = new BufferedImage(img.getWidth(), img.getHeight(), 2);
         int crgba = c.getRGB();
-        for(int x = 0; x < getWidth(); x++){
-            for(int y = 0; y < getHeight(); y++){
-                int rgba = img.getRGB(x,y);
-                int a = (rgba & 0xff000000) >>> 24;
+        int[][] argbIntArray = toARGBIntArray(img);
+        for(int x = 0; x < argbIntArray.length; x++){
+            for(int y = 0; y < argbIntArray[0].length; y++){
+                int argb = argbIntArray[x][y];
+                int a = (argb & 0xff000000) >>> 24;
                 if(a != 255){
                     out.setRGB(x,y, 0x00000000);
                 }else{
